fa55de88135ba74f526c7f51fe735459d2931a55
zjani4005-eng <zjani4005@gmail.com>
2026-08-16 00:27:53 +0500

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://wcjrywbzmjmbusefqvqj.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_k2Vku-pR3PJchI2X37NZEw_CifYZ7t6";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
